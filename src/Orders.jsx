3d65d6ced3e82fc5d946488043092c88fe9b5a6a
mnmehr04 <157523751+mnmehr04@users.noreply.github.com>
2026-01-18 07:13:22 +0530

    Update header link to point to root URL

--- src/Orders.jsx
+++ src/Orders.jsx
@@ -21,7 +21,7 @@ export function Orders() {
     <>
       <div className="header">
         <div className="left-section">
-          <a href="index.html" className="header-link">
+          <a href="/" className="header-link">
             <img className="logo" src="images/logo-white.png" />
             <img className="mobile-logo" src="images/mobile-logo-white.png" />
           </a>
